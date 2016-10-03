fc27bc1a7aa76dd9f6694427e770d8db79ee530e
Matthew Marshall <mamarshall44@gmail.com>
2016-10-03 14:40:29 -0700

    Ace of Hearts Layouts finished

---
+++ CardStack/app/src/main/java/edu/chapman/cpsc356/cardstack/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.chapman.cpsc356.cardstack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
